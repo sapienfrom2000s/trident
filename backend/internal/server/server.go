6b49c407e49c852cfbc347feff244a6a977abc6a
thirtyone <thirtyone@thirtyone.local>
2026-04-09 21:15:17 +0530

    test heartbeat API

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -1,1 +1,10 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func HeartBeatHandler(h http.ResponseWriter, r *http.Request) error {
+	return fmt.Errorf("No implementation error")
+}
--- backend/internal/server/server_test.go
+++ backend/internal/server/server_test.go
@@ -1,1 +1,29 @@
 package server_test
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/sapienfrom2000s/trident/backend/internal/server"
+)
+
+func TestAgentHeartBeat(t *testing.T) {
+	payload := []byte(`
+		{
+			"timestamp": "some-time",
+		}
+    `)
+	body := bytes.NewBuffer(payload)
+	// As of now security issue to accept heartbeat w/o any authentication
+	req := httptest.NewRequest(http.MethodPost, "/heartbeat", body)
+	rec := httptest.NewRecorder()
+
+	err := server.HeartBeatHandler(rec, req)
+	t.Run("Healthy Hearbeat", func(t *testing.T) {
+		if err != nil {
+			t.Errorf("Expected: No Error, Got: %v", err)
+		}
+	})
+}
